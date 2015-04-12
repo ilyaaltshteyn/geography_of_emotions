c76debe8353ea9244810f3437fe62d6edd2f7052
ilya <ilyaaltshteyn@gmail.com>
2015-04-12 15:24:50 -0400

    add scraper script

---
+++ scrape words.py
@@ -0,0 +1,7 @@
+#This script scrapes the last words and offender information summaries of 
+#people executed on death row between 1982 and present time from the texas 
+#department of criminal justice website.
+
+import urllib
+
+url = http://www.tdcj.state.tx.us/death_row/dr_executed_offenders.html
